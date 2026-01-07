5d7b15d9f837291e16d8f45771e5e5f45caf07f9
Archita Sindigi <archita.g.sindigi@gmail.com>
2026-01-07 09:38:05 +0530

    exercise 1.15:rewrote the temperature convertion program to use function for conversion

--- Chapter 1/temperature.c
+++ Chapter 1/temperature.c
@@ -4,24 +4,38 @@
 # define UPPER  300
 # define STEP  20
 
-main(){
-    int f;
-    float c;
+int converter(int l, int u, int s);
+
+int main(void){
+    // int f;
+    // float c;
     //int lower, upper, step;
 
     // lower = 0;
     // upper = 300;
     // step = 20;
 
-    f = UPPER;
+    // f = UPPER;
 
     printf("Welcome to the Fahrenheit to Celsius converter\n");
-    while(f>=LOWER){
-        c = (5.0/9.0)*(f - 32.0);
-        printf("%3d %6.1f\n",f,c);
-        f-=STEP;
-    }
+    converter(LOWER, UPPER, STEP);
+    // while(f>=LOWER){
+    //     c = (5.0/9.0)*(f - 32.0);
+    //     printf("%3d %6.1f\n",f,c);
+    //     f-=STEP;
+    // }
     /*for(f = 0; f<=upper ; f+=20){
         printf("%3d %6.1f\n",f,(5.0/9.0)*(f - 32));
     }*/
+   return 0;
+}
+
+int converter(int l, int u, int s){
+    int f;
+    float c;
+    while(f<=u){
+        c = (5.0/9.0)*(f - 32.0);
+        printf("%3d %6.1f\n", f,c);
+        f+=s;
+    }
 }
